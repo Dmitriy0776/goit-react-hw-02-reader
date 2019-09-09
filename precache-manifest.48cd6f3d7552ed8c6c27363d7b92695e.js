331d935cfa66d6ddadc8291cdf34a2d464669c1e
Dmitriy <velkov55@gmail.com>
2019-09-09 17:56:24 +0300

    Updates

---
+++ precache-manifest.48cd6f3d7552ed8c6c27363d7b92695e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "74b8615f2725b6623dec855af5e6c034",
+    "url": "/goit-react-hw-02-reader/index.html"
+  },
+  {
+    "revision": "39209bbca49059fb888f",
+    "url": "/goit-react-hw-02-reader/static/css/main.7df1979f.chunk.css"
+  },
+  {
+    "revision": "68017f9caf930895b515",
+    "url": "/goit-react-hw-02-reader/static/js/2.d673c151.chunk.js"
+  },
+  {
+    "revision": "39209bbca49059fb888f",
+    "url": "/goit-react-hw-02-reader/static/js/main.1df7ffb0.chunk.js"
+  },
+  {
+    "revision": "6ad384aa03afd857de17",
+    "url": "/goit-react-hw-02-reader/static/js/runtime~main.1bfe6297.js"
+  }
+]);
